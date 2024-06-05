baea071ea5f2b0d45c5cf295601c02e0785c8782
Sumair Syed <ssy@SVMBP15A.local>
2024-06-05 13:40:48 +1000

    Updated project.json file to use aws amplify authenticator styles and to use the AWS authenticator module

--- apps/todo-app-amplify-gen-2/src/app/app.config.ts
+++ apps/todo-app-amplify-gen-2/src/app/app.config.ts
@@ -1,9 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(BrowserAnimationsModule, AmplifyAuthenticatorModule),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
   ],
